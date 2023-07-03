d33e9135c0092b600af5230abe7937310ecfa2dd
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-03 15:13:33 +0200

    Added Header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script></script>
 
-<template></template>
+<template>
+    <header>
+        <div class="container">
+            <div class="my-navbar my-5">
+                <div class="circle big-circle blue-circle me-2"></div>
+                <div class="circle small-circle red-circle  mx-2"></div>
+                <div class="circle small-circle yellow-circle  mx-2"></div>
+                <div class="circle small-circle green-circle  mx-2"></div>
+            </div>
+        </div>
+    </header>
+</template>
 
 <style lang="scss">
 @use "./assets/scss/style.scss" as *;
