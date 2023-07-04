95ff9edac4fbf984b7b97e02cbba5e84ab3fcb80
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-04 16:09:15 +0200

    Added Data and Select

---
+++ src/components/components-Header/SelectHeader.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+            optionSelected: ""
+        }
+    },
+    props: {
+        options: Array
+    },
+    emits: ["option"]
+}
+</script>
+<template>
+    <div class="container mb-5">
+        <select class="form-select" v-model="optionSelected" @change="$emit('option', optionSelected)">
+            <option selected value="">---</option>
+            <option :value="option" v-for="option in options">{{ option }}</option>
+        </select>
+    </div>
+</template>
+<style scoped lang="scss">
+select {
+    width: 500px;
+}
+</style>
