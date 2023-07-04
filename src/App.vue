920893297679b4d3dd9167ac3042443ba1e4ee8c
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-07-04 15:18:29 +0200

    Added AppHeader

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 const endpoint = "https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons"
 import axios from "axios";
 import { store } from "./assets/data/store"
+import AppHeader from "./components/AppHeader.vue"
 import AppMain from "./components/AppMain.vue"
 import AppLoader from "./components/AppLoader.vue"
 export default {
-    components: { AppMain, AppLoader },
+    components: { AppMain, AppLoader, AppHeader },
     mounted() {
         axios.get(endpoint).then(res => {
             store.isLoaded = false
@@ -17,16 +18,7 @@ export default {
 </script>
 
 <template>
-    <header>
-        <div class="container">
-            <div class="my-navbar my-5">
-                <div class="circle big-circle blue-circle me-2"></div>
-                <div class="circle small-circle red-circle  mx-2"></div>
-                <div class="circle small-circle yellow-circle  mx-2"></div>
-                <div class="circle small-circle green-circle  mx-2"></div>
-            </div>
-        </div>
-    </header>
+    <AppHeader />
     <AppMain />
     <AppLoader />
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="my-navbar my-5">
+                <div class="circle big-circle blue-circle me-2"></div>
+                <div class="circle small-circle red-circle  mx-2"></div>
+                <div class="circle small-circle yellow-circle  mx-2"></div>
+                <div class="circle small-circle green-circle  mx-2"></div>
+            </div>
+        </div>
+    </header>
+</template>
+<style scoped lang="scss">
+.circle {
+    border-radius: 50%;
+}
+
+.big-circle {
+    width: 100px;
+    height: 100px;
+    border: 5px solid white;
+}
+
+.small-circle {
+    width: 50px;
+    height: 50px;
+    border: 3px solid white;
+
+}
+
+.blue-circle {
+    background-image: linear-gradient(128deg, #ffffff 10%, #0000ff 70%);
+
+}
+
+.red-circle {
+    background-image: linear-gradient(128deg, #ffffff 10%, #ff0000 70%);
+}
+
+.yellow-circle {
+    background-image: linear-gradient(128deg, #ffffff 10%, #ffff00 70%);
+}
+
+.green-circle {
+    background-image: linear-gradient(128deg, #ffffff 10%, #008000 70%);
+}
+</style>
